2b38f610644c10d37c3cfc08b8c0e04c811263f8
ChuanHao Wang <absir@qq.com>
2014-07-30 18:28:18 +0800

    update

---
+++ absir-developer/src/main/java/com/absir/appserv/lang/LangBundleImpl.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2014 ABSir's Studio
+ * 
+ * All right reserved
+ *
+ * Create on 2014年7月29日 下午5:15:42
+ */
+package com.absir.appserv.lang;
+
+import java.io.File;
+
+import com.absir.appserv.support.Developer;
+import com.absir.bean.basis.Base;
+import com.absir.bean.core.BeanConfigImpl;
+import com.absir.bean.inject.value.Bean;
+import com.absir.bean.inject.value.Stopping;
+import com.absir.context.lang.LangBundle;
+import com.absir.core.kernel.KernelString;
+
+/**
+ * @author absir
+ *
+ */
+@Base(order = -1)
+@Bean
+public class LangBundleImpl extends LangBundle {
+
+	/**
+	 * 内置国际化资源写入
+	 */
+	@Stopping
+	public void stopping() {
+		if (!resourceLangs.isEmpty()) {
+			String var = locale.getLanguage();
+			if (!KernelString.isEmpty(var)) {
+				String resource = langResource + var;
+				var = locale.getCountry();
+				if (!KernelString.isEmpty(var)) {
+					resource += '_' + var;
+					var = locale.getVariant();
+					if (!KernelString.isEmpty(var)) {
+						resource += '_' + var;
+					}
+				}
+
+				File file = new File(resource + "/general.properties");
+				BeanConfigImpl.writeProperties(resourceLangs, file);
+				Developer.doEntry(file);
+			}
+		}
+	}
+}
